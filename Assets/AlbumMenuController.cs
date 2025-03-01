0f81ca3736a02e4d5137ecc06102bf2a29a2a2b8
Elian Melo <elianmelomorais@gmail.com>
2025-03-01 20:27:03 -0300

    Start creating album menu

---
+++ Assets/AlbumMenuController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlbumMenuController : MonoBehaviour
+{
+    public static string DefaultTrackValue = "01 - -----------------------";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
